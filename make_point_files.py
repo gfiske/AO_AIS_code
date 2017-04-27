33ce3c9910d2153c19732489e29e2550db0e70be
gfiske <gfiske@whrc.org>
2017-04-27 15:41:25 -0400

    redo for ancillary data

--- make_point_files.py
+++ make_point_files.py
@@ -2,25 +2,29 @@
 
 #An ArcGIS script that'll convert all csv files to feature layers in a FGDB
 #gfiske Feb 2017
+#updated on Apr 24, 2017 to run on the ancillary data
 
 import arcpy,os
+#from arcpy.sa import *
 
 # Set the current workspace
-arcpy.env.workspace = "C:/Data/Arctic/ArcticOptions/temp_csv"
-outdir = "C:/Data/Arctic/ArcticOptions/AO_AIS_Feb2017.gdb/pointData/"
+arcpy.env.workspace = "C:/Data/Crap/AIS_backup_March2017/ancillary_data"
+outdir = "C:/Data/Arctic/ArcticOptions/AIS_w_ancillary_data_Apr_2017.gdb/pointData"
+
+# Set the local variables
+x_coords = "long"
+y_coords = "lat"
 
 
-# Copy each file with a .csv extension to a dBASE file
+# Convert each .csv file to a feature class
 for csv_file in arcpy.ListFiles("*.csv"):
     # Use splitext to set the output table name
     out_file = os.path.splitext(csv_file)[0] 
-    out_file = out_file.replace(".", "_")
-    out_file = out_file.replace("-", "_")
-    #print out_file
-    # Set the local variables
-    x_coords = "x"
-    y_coords = "y"
-    # Make the XY event layer...
-    arcpy.MakeXYEventLayer_management(csv_file, x_coords, y_coords, out_file, "", "")    
-    # Write layer to feature class
-    arcpy.FeatureClassToFeatureClass_conversion(out_file, outdir, out_file)
+    out_file = "p" + out_file.replace("-", "_")
+    # Make the XY event layer...    
+    try:
+        arcpy.MakeXYEventLayer_management(csv_file, x_coords, y_coords, out_file, "", "")    
+        # Write layer to feature class
+        arcpy.FeatureClassToFeatureClass_conversion(out_file, outdir, out_file)
+    except:
+        print "this file failed: " + out_file
