4a5d4fa32a6a8963388f326468ef96f41c782dce
gfiske <gfiske@whrc.org>
2017-06-08 09:08:40 -0400

    initial add

--- AIS_to_CSV_with_no_spatial.R
+++ AIS_to_CSV_with_no_spatial.R
@@ -1,27 +1,33 @@
 #AIS_to_CSV_with_null_spatial.R
 
 #The purpose of this script is to count how many records in the raw "|" AIS data have no spatial location information
+#gfiske May 2017
 
 #set input output
 rm(list = ls())
 #indir <- "/mnt/gcs-bucket/decoded/"
-indir <- "C:/Data/Crap/AIS_backup_March2017/decoded/"
+indir <- "/mnt/gcs-bucket/decoded/ancillary_data/"
 #outdir <- "/mnt/gcs-bucket/csv/"  
 
-# find all psv files
-psvfiles <- list.files(indir, "*.psv$", full.names = T)
+#read one table as a test
+#mydata = read.csv("/mnt/gcs-bucket/decoded/ancillary_data/2011-01-15-p0001.csv", header=T, sep=",")
+
+# find all the csv files
+csvfiles <- list.files(indir, "*.csv", full.names = T)
 
 #keep a count
 mycount <- 0
 
 # Loop through each file and count records with no spatial info
-for (i in psvfiles) {
-  mydata = read.csv(i, header=T, sep="|")
+for (i in csvfiles) {
+  mydata = read.csv(i, header=T, sep=",")
   #subset only the pertinent fields
-  myvars <- c("mmsi", "rot", "satellite_timestamp", "true_heading", "x", "y")
+  myvars <- c("time","mmsi","nav","rot","truehead","long","lat","name","destination","type","length","width","draught","mid","flag")
   newdata <- subset(mydata[myvars])
   #identify records with non-spatial records
-  newdata <- subset(newdata, newdata$x == 181.000000)
+  newdata <- subset(newdata, newdata$long = 181.000000)
+  #or with spatial records
+  #newdata <- subset(newdata, newdata$long < 181.000000)
   mycount <- nrow(newdata) + mycount
   }
 
