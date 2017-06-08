2e04a19b5d6987cd3fbfec2d6d6bb41417c4c461
gfiske <gfiske@whrc.org>
2017-06-08 09:05:05 -0400

    clean for sharing

--- merge_csv.py
+++
@@ -1,8 +0,0 @@
-import glob, csv, sys
-filenames = glob.glob("C:/Data/Crap/test/*.csv")
-with open('C:/Data/Crap/test/output/output.csv', 'a') as oneFile:
-          for i in filenames:
-                    with open(i, 'r') as f:
-                              for row in f:
-                                        oneFile.write(row)
-    
