8367f37c919e3b16af79fb241bd63e151f14c208
gfiske <gfiske@whrc.org>
2017-06-14 08:23:52 -0400

    allows rerun from fail

--- spatialJoin_by_day_SeaIce.py
+++ spatialJoin_by_day_SeaIce.py
@@ -11,9 +11,10 @@ import arcpy,os
 arcpy.env.workspace = "C:/Data/Arctic/ArcticOptions/AIS_w_ancillary_data_Apr_2017.gdb"
 arcpy.env.overwriteOutput = True
 
-#choose a year
-year = str(2015)
+#choose a year set output directory
+year = str(2014)
 inTable = "spJoined" + year
+outdir = "C:\\Data\\Arctic\\ArcticOptions\\AIS_w_ancillary_data_Apr_2017.gdb\\iceJoined" + year + "\\"
 
 #make a JD range
 jd = range(1,365)
@@ -22,24 +23,25 @@ jd = range(1,365)
 for i in jd:
     i = (format(i, '03'))
     yearJD = year + i
-    #block into try except due to potential errors
-    try:
-        # Make a layer from the feature class
-        arcpy.MakeFeatureLayer_management(inTable, "lyr")        
-        #select out all the points for one jd
-        arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", ' "yd" = ' + yearJD)
-        #from that subset do a spatial join to the ice data
-        outdir = "C:\\Data\\Arctic\\ArcticOptions\\AIS_w_ancillary_data_Apr_2017.gdb\\iceJoined" + year + "\\"
-        arcpy.analysis.SpatialJoin("lyr", "C:\\Users\\gfiske\\Downloads\\" + year + "\\masie_ice_r00_v01_" + yearJD +"_4km.shp", outdir + "ice_" + yearJD, "JOIN_ONE_TO_MANY", "KEEP_COMMON", None, "INTERSECT", None, None)
-        #the output joined feature class will have the attributes of the NSIDC sea ice data shapefile
-    except:
-        print "this jd failed: " + i
-        file = open('C:\\Data\\Arctic\\ArcticOptions\\seaIce_NSIDC_fail_log.txt', 'a')
-        file.write("YearJD fail: %s" % yearJD)
-        file.write("\n")
-        file.close()
-        #some jd have no NSIDC data
-        #other errors are yet to be determined
+    #only produce those that don't exist
+    if not arcpy.Exists(outdir + "ice_" + yearJD):
+        #block into 'try except' due to potential errors
+        try:
+            # Make a layer from the feature class
+            arcpy.MakeFeatureLayer_management(inTable, "lyr")        
+            #select out all the points for one jd
+            arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", ' "yd" = ' + yearJD)
+            #from that subset do a spatial join to the ice data
+            arcpy.analysis.SpatialJoin("lyr", "C:\\Users\\gfiske\\Downloads\\" + year + "\\masie_ice_r00_v01_" + yearJD +"_4km.shp", outdir + "ice_" + yearJD, "JOIN_ONE_TO_MANY", "KEEP_COMMON", None, "INTERSECT", None, None)
+            #the output joined feature class will have the attributes of the NSIDC sea ice data shapefile
+        except:
+            print "this jd failed: " + i
+            file = open('C:\\Data\\Arctic\\ArcticOptions\\seaIce_NSIDC_fail_log.txt', 'a')
+            file.write("YearJD fail: %s" % yearJD)
+            file.write("\n")
+            file.close()
+            #some jd have no NSIDC data
+            #other errors are yet to be determined
 
 
     
